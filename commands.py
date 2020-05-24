b254f7bbbbd2583b1d906ae87556cf172285494e
eelyel <ostanley.lee@gmail.com>
2020-05-24 17:58:33 -0400

    Replaced lambda with partial; moved search function for clarity

--- commands.py
+++ commands.py
@@ -4,6 +4,7 @@ from typing import List
 from random import randint
 import discord
 from log import logger
+from searches import search
 import searches
 import re
 import requests
@@ -12,20 +13,21 @@ from subprocess import Popen
 import uuid
 import os
 import asyncio
+from functools import partial
 
 
 ALL_COMMANDS = {
     'help': lambda *_: show_help(),
     'kill': None,
     'roll': lambda _, inputs, cid: roll(inputs),
-    searches.SCP: lambda args, inputs, cid: search(args, inputs, searches.SCP, cid),
-    searches.STEAM: lambda args, inputs, cid: search(args, inputs, searches.STEAM, cid),
-    searches.MAL: lambda args, inputs, cid: search(args, inputs, searches.MAL, cid),
-    searches.MD: lambda args, inputs, cid: search(args, inputs, searches.MD, cid),
-    searches.MU: lambda args, inputs, cid: search(args, inputs, searches.MU, cid),
-    searches.NU: lambda args, inputs, cid: search(args, inputs, searches.NU, cid),
-    searches.WIKI: lambda args, inputs, cid: search(args, inputs, searches.WIKI, cid),
-    searches.XKCD: lambda args, inputs, cid: search(args, inputs, searches.XKCD, cid),
+    searches.SCP: partial(search, searches.SCP),
+    searches.STEAM: partial(search, searches.STEAM),
+    searches.MAL: partial(search, searches.MAL),
+    searches.MD: partial(search, searches.MD),
+    searches.MU: partial(search, searches.MU),
+    searches.NU: partial(search, searches.NU),
+    searches.WIKI: partial(search, searches.WIKI),
+    searches.XKCD: partial(search, searches.XKCD),
 }
 
 BUFFER_MESSAGES = Buffer_List()
@@ -106,66 +108,3 @@ def roll(inputs: List[str]) -> str:
     # !roll Alice Bob Charlie - random string list rolling
     return inputs[randint(0, len(inputs) - 1)]
 
-def search(args: List[str], inputs: List[str], search_type: str, channel_id: int) -> discord.Embed:
-    """
-    Return a formatted string from the given query in inputs and the search type.
-    """
-    logger.info("Searching - args: %s | inputs: %s | type: %s", args, inputs, search_type)
-
-
-    # default to one to prevent the bot's messages from taking too much space in chat
-    # also notes if the result is fixed; by default it is not
-    num_results = (1, False)
-
-    # Look for any numerical argument passed in; arbitrarily choose one
-    # it will represent the number of search results to return (it will never be > 10)
-    # also look for ^, the character for indicating we should use recent messages from the chat
-    # look also for 'r': this represents a random number between 1-5000 - specifically for SCP
-    for arg in args:
-        if arg.isdigit() and not num_results[1]:
-            arg_int = int(arg)
-            # capped results at 5 to prevent more than 2000 chars being sent at once (400 exception)
-            num_results = (5, False) if arg_int > 5 else (arg_int, False)
-            logger.info("Changing number of search results to %i", num_results[0])
-        if 'r' in arg:
-            ran = randint(1, 5000)
-            # allow results to be 3
-            num_results = (3, True)
-            inputs.append(str(ran))
-            logger.info("Appended %i to inputs: %s", ran, inputs)
-        if arg.startswith('^'):
-            logger.info("Searching via past messages")
-            try:
-                inputs = BUFFER_MESSAGES.get(channel_id, int(arg[1]))
-                logger.info("Searching via past message of %s", inputs)
-            except IndexError:
-                # if indeed the ^ command is accompanied with no additional argument
-                inputs = BUFFER_MESSAGES.get(channel_id, 0)
-                logger.info("Failed to retrieved specified message, now searching for %s", inputs)
-
-    inputs = ' '.join(inputs)
-    logger.info("post-process - args: %s | inputs: %s | type: %s", args, inputs, search_type)
-
-    results = searches.google_search(inputs, num_results[0], search_type)
-
-    if not results:
-        return "No results found - please verify spelling"
-
-    displayed_result = ''
-
-    # Formatting looks as follows:
-    # >>> !wiki apple
-    #
-    # Apple Inc. - Wikipedia
-    #
-    # Apple Inc. is an American multinational technology company headquartered in
-    # Cupertino, California, that designs, develops, and sells consumer electronics, ...
-    #
-    # https://en.wikipedia.org/wiki/Apple_Inc
-    for result in results:
-        title = result['title']
-        snippet = result['snippet']
-        link = result['link']
-        displayed_result += "**{}**\n{}\n{}\n\n".format(title, snippet, link)
-
-    return discord.Embed(title="Top search results", description=displayed_result)
--- searches.py
+++ searches.py
@@ -2,6 +2,8 @@
 from typing import List
 import os
 import requests
+import discord
+from log import logger
 
 MAL = 'mal'
 MD = 'md'
@@ -44,3 +46,67 @@ def google_search(search_query: str, num_results: int, search_type: str) -> List
         return []
 
     return res['items']
+
+def search(search_type: str, args: List[str], inputs: List[str], channel_id: int) -> discord.Embed:
+    """
+    Return a formatted string from the given query in inputs and the search type.
+    """
+    logger.info("Searching - args: %s | inputs: %s | type: %s", args, inputs, search_type)
+
+
+    # default to one to prevent the bot's messages from taking too much space in chat
+    # also notes if the result is fixed; by default it is not
+    num_results = (1, False)
+
+    # Look for any numerical argument passed in; arbitrarily choose one
+    # it will represent the number of search results to return (it will never be > 10)
+    # also look for ^, the character for indicating we should use recent messages from the chat
+    # look also for 'r': this represents a random number between 1-5000 - specifically for SCP
+    for arg in args:
+        if arg.isdigit() and not num_results[1]:
+            arg_int = int(arg)
+            # capped results at 5 to prevent more than 2000 chars being sent at once (400 exception)
+            num_results = (5, False) if arg_int > 5 else (arg_int, False)
+            logger.info("Changing number of search results to %i", num_results[0])
+        if 'r' in arg:
+            ran = randint(1, 5000)
+            # allow results to be 3
+            num_results = (3, True)
+            inputs.append(str(ran))
+            logger.info("Appended %i to inputs: %s", ran, inputs)
+        if arg.startswith('^'):
+            logger.info("Searching via past messages")
+            try:
+                inputs = BUFFER_MESSAGES.get(channel_id, int(arg[1]))
+                logger.info("Searching via past message of %s", inputs)
+            except IndexError:
+                # if indeed the ^ command is accompanied with no additional argument
+                inputs = BUFFER_MESSAGES.get(channel_id, 0)
+                logger.info("Failed to retrieved specified message, now searching for %s", inputs)
+
+    inputs = ' '.join(inputs)
+    logger.info("post-process - args: %s | inputs: %s | type: %s", args, inputs, search_type)
+
+    results = google_search(inputs, num_results[0], search_type)
+
+    if not results:
+        return "No results found - please verify spelling"
+
+    displayed_result = ''
+
+    # Formatting looks as follows:
+    # >>> !wiki apple
+    #
+    # Apple Inc. - Wikipedia
+    #
+    # Apple Inc. is an American multinational technology company headquartered in
+    # Cupertino, California, that designs, develops, and sells consumer electronics, ...
+    #
+    # https://en.wikipedia.org/wiki/Apple_Inc
+    for result in results:
+        title = result['title']
+        snippet = result['snippet']
+        link = result['link']
+        displayed_result += "**{}**\n{}\n{}\n\n".format(title, snippet, link)
+
+    return discord.Embed(title="Top search results", description=displayed_result)
